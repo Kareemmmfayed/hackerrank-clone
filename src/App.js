51b732b088fdcb7ee57cf75275cde504eadc1e78
kareem <124769561+Kareemmmfayed@users.noreply.github.com>
2023-08-29 05:18:21 +0300

    Landing page done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header'
 import Home from './components/Home'
+import Slides from './components/Slides'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
+      <Slides />
     </div>
   );
 }
---
+++ src/components/Slides.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Slides() {
+    return (
+        <div className='slides'>
+            <h2>It's not a pipeline problem.</h2>
+            <h2>It's a spotlight problem.</h2>
+            <p>Tech hiring needs a reset. From prepping for jobs and practicing coding to running a world-class technical interview,
+                give developers the tools they need to showcase their skills, passion, and potential.</p>
+            <nav>
+                <a href='#prep'>Prep</a>
+                <a href='#screen'>Screen</a>
+                <a href='#interview'>Interview</a>
+            </nav>
+            
+        </div>
+    )
+}
+
+export default Slides
