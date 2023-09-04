66ffd7fc0b339386151f3dc9e9b1f13343c0b202
kareem <124769561+Kareemmmfayed@users.noreply.github.com>
2023-09-04 07:53:43 +0300

    Finished the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,65 @@
 import React from 'react'
+import kr from '../assets/favicon.png'
 
 function Footer() {
     return (
-        <div className='footer'>Footer</div>
+        <div className='footer'>
+            <div>
+                <h4>PRODUCTS</h4>
+                    <ul>
+                        <li>Screen</li>
+                        <li>Interview</li>
+                        <li>Certified Assessments</li>
+                        <li>Plagiarism Detection</li>
+                        <li>HackerRank AI</li>
+                    </ul>
+            </div>
+            <div>
+                <h4>SOLUTIONS</h4>
+                    <ul>
+                        <li>Enterprise Technical Hiring</li>
+                        <li>Remote Hiring</li>
+                        <li>University Recruiting</li>
+                        <li>Diversity & Inclusion</li>
+                        <li>Professional Hiring</li>
+                        <li>Professional Services</li>
+                    </ul>
+            </div>
+            <div>
+                <h4>RESOURCES</h4>
+                    <ul>
+                        <li>Blog</li>
+                        <li>Customer Stories</li>
+                        <li>Events and Webinars</li>
+                        <li>Roles Directory</li>
+                        <li>Hiring Guides</li>
+                        <li>What's New</li>
+                    </ul>
+            </div>
+            <div>
+                <h4>ABOUT US</h4>
+                    <ul>
+                        <li>Careers</li>
+                        <li>Our Team</li>
+                        <li>Newsroom</li>
+                        <li>Security and Compliance</li>
+                        <li>Status</li>
+                        <li>Partners</li>
+                        <li>Integrations</li>
+                    </ul>
+            </div>
+            <div>
+                <h4>GET STARTED</h4>
+                    <ul>
+                        <li>Pricing</li>
+                        <li>Request Demo</li>
+                        <li>Free Trial</li>
+                        <li>Contact Us</li>
+                        <li>Product Support</li>
+                        <li>For Developers</li>
+                    </ul>
+            </div>
+        </div>
     )
 }
 
